688256107e567b9e26329baa564fe974facf46b1
Theo Julienne <theo.julienne@gmail.com>
2006-08-17 08:47:37 +0000

    fonts works

--- src/options/opt_fonts.c
+++ src/options/opt_fonts.c
@@ -37,7 +37,7 @@ typedef struct
 	object_t *button;
 } BOptionFontItem;
 
-BOptionFontItem items[] = {
+BOptionFontItem bof_items[] = {
 	{ "opt_font_server_buffer", "server-buffer", "", "", 0, 0 },
 	{ "opt_font_server_input", "server-input", "", "", 0, 0 },
 	{ "opt_font_chat_buffer", "chat-buffer", "", "", 0, 0 },
@@ -47,41 +47,54 @@ BOptionFontItem items[] = {
 
 void b_opt_font_update_display( int a )
 {
-	sprintf( items[a].display, "%s, %d", items[a].face, items[a].size );
+	sprintf( bof_items[a].display, "%s, %d", bof_items[a].face, bof_items[a].size );
 }
 
 int b_fontd_by_loc_from_xml( BFontDef *font, char *loc );
 
-void b_opt_font_choose_click( CEvent *e, void *data )
+extern object_t *optionswin;
+
+event_handler( b_opt_font_choose_response )
 {
-#if 0 /* PORTHACK */
-	int a, b;
+	font_t *f;
+	int accept = event_get_arg_int( event, 0 );
+	int a = (int)object->appdata;
+	
+	if ( accept )
+	{
+		f = font_dialog_get_font( object );
+		
+		strcpy( bof_items[a].face, f->face );
+		bof_items[a].size = f->size;
+		
+		b_opt_font_update_display( a );
+		textbox_set_text( bof_items[a].input, bof_items[a].display );
+	}
+	
+	widget_close( object );
+}
+
+event_handler( b_opt_font_choose_click )
+{
+	int a;
 	
 	for ( a = 0; ; a++ )
 	{
-		if ( !strcmp( items[a].name, "" ) )
+		if ( !strcmp( bof_items[a].name, "" ) )
 			return;
 		
-		if ( e->caller == items[a].button )
+		if ( object == bof_items[a].button )
 			break;
 	}
 	
-	c_choosefont_set_font_face( opt_font_select, items[a].face );
-	c_choosefont_set_font_size( opt_font_select, items[a].size );
+	opt_font_select = font_dialog_widget_create( optionswin, 0 );
+	opt_font_select->appdata = (void *)a;
 	
-	b = c_choosefont_display( opt_font_select );
+	object_addhandler( opt_font_select, "response", b_opt_font_choose_response );
 	
-	if ( b == 0 )
-	{
-		return;
-	}
+	font_dialog_set_font( opt_font_select, bof_items[a].face, bof_items[a].size, 0, 0, 0 );
 	
-	strcpy( items[a].face, c_choosefont_get_font_face( opt_font_select ) );
-	items[a].size = c_choosefont_get_font_size( opt_font_select );
-	
-	b_opt_font_update_display( a );
-	c_text_settext( items[a].input, items[a].display );
-#endif
+	widget_show( opt_font_select );
 }
 
 B_OPTIONS_PAGE_CREATE( b_options_font_page )
@@ -94,42 +107,30 @@ B_OPTIONS_PAGE_CREATE( b_options_font_page )
 	
 	cat = c_new_groupbox( parent, title, px, py, pw, ph, 0 );
 
-	//opt_font_select = c_new_choosefont( parent );
-
 	for ( a = 0; ; a++ )
 	{
-		if ( !strcmp( items[a].name, "" ) )
+		if ( !strcmp( bof_items[a].name, "" ) )
 			break;
 		
-		c_new_label( cat, lang_phrase_quick( items[a].name ), 0, y, sx, -1, 0 );
-		b_fontd_by_loc_from_xml( &fontd, items[a].cfg );
+		c_new_label( cat, lang_phrase_quick( bof_items[a].name ), 0, y, sx, -1, 0 );
+		b_fontd_by_loc_from_xml( &fontd, bof_items[a].cfg );
 		
-		strcpy( items[a].face, fontd.face );
-		items[a].size = fontd.size;
+		strcpy( bof_items[a].face, fontd.face );
+		bof_items[a].size = fontd.size;
 		
 		b_opt_font_update_display( a );
 		
-		items[a].input = textbox_widget_create( cat, new_bounds(sx, y, pw-20-sx-110, -1), 0 );
-		textbox_set_text( items[a].input, items[a].display );
-		items[a].button = c_new_pushbutton( cat, lang_phrase_quick( "choose" ), pw-120, y, 100, -1, 0 );
+		bof_items[a].input = textbox_widget_create( cat, new_bounds(sx, y, pw-20-sx-110, -1), 0 );
+		textbox_set_text( bof_items[a].input, bof_items[a].display );
+		bof_items[a].button = c_new_pushbutton( cat, lang_phrase_quick( "choose" ), pw-120, y, 100, -1, 0 );
 		
-		widget_disable( items[a].input );
+		widget_disable( bof_items[a].input );
 		
-		object_addhandler( items[a].button, "pushed", b_opt_font_choose_click );
+		object_addhandler( bof_items[a].button, "pushed", b_opt_font_choose_click );
 		
 		y += 30;
 	}
-
-	/*
-	c_new_label( cat, "Server buffer :", 10, y, sx, -1, 0 );
-	b_fontd_by_loc_from_xml( &fontd, "server-buffer" );
-	opt_font_server_buffer_font = fontd.face;
-	opt_font_server_buffer_size = fontd.size;
-	opt_font_server_buffer = c_new_text( cat, b_opt_font_get_str( opt_font_server_buffer_font, opt_font_server_buffer_size ), sx+10, y, pw-20-sx-110, -1, 0 );
-	opt_font_server_buffer_btn = c_new_pushbutton( cat, lang_phrase_quick( "choose" ), pw-110, y, 100, -1, 0 );
-	c_widget_disable( opt_font_server_buffer );
-	y += 30;
-	*/
+	
 	return cat;
 }
 
@@ -137,15 +138,14 @@ extern BServerWindow *server_head;
 
 void b_opt_fonts_set_font( XMLItem *p, int a )
 {
-#if 0 /* PORTHACK */
 	XMLItem *fonts;
 	char *xlocn;
-	//char *size, *face;
 	char tmp[32];
 	BServerWindow *swin;
 	BChatWindow *cwin;
+	node_t *n, *cn;
 	
-	sprintf( tmp, "%d", items[a].size );
+	sprintf( tmp, "%d", bof_items[a].size );
 	
 	for ( fonts = p->child_head; fonts; fonts = fonts->next )
 	{
@@ -158,35 +158,41 @@ void b_opt_fonts_set_font( XMLItem *p, int a )
 		if ( xlocn == 0 )
 			continue;
 		
-		if ( !strcasecmp( xlocn, items[a].cfg ) )
+		if ( !strcasecmp( xlocn, bof_items[a].cfg ) )
 		{
-			c_xml_attrib_set( fonts, "face", items[a].face );
+			c_xml_attrib_set( fonts, "face", bof_items[a].face );
 			c_xml_attrib_set( fonts, "size", tmp );
 						
-			goto b_o_f_s_f_finish;
+			goto update_fonts;
 		}
 	}
 	
 	fonts = c_xml_add_child( p, "font" );
 	
-	c_xml_attrib_set( fonts, "location", items[a].cfg );
-	c_xml_attrib_set( fonts, "face", items[a].face );
+	c_xml_attrib_set( fonts, "location", bof_items[a].cfg );
+	c_xml_attrib_set( fonts, "face", bof_items[a].face );
 	c_xml_attrib_set( fonts, "size", tmp );
 	
-	b_o_f_s_f_finish:
+	update_fonts:
 	
-	for ( swin = server_head; swin != 0; swin = swin->next )
+	LIST_FOREACH( n, bersirc->servers.head )
 	{
-		b_widget_set_font( swin->content->w, "server-buffer" );
-		b_widget_set_font( swin->input, "server-input" );
+		swin = (BServerWindow *)n->data;
 		
-		for ( cwin = swin->chat_head; cwin != 0; cwin = cwin->next )
+		b_widget_set_font( swin->content, "server-buffer" );
+		b_widget_set_font( swin->input, "server-input" );
+		canvas_redraw( swin->content );
+		
+		LIST_FOREACH( cn, swin->chat_windows.head )
 		{
-			b_widget_set_font( cwin->content->w, "chat-buffer" );
+			cwin = (BChatWindow *)cn->data;
+			
+			b_widget_set_font( cwin->content, "chat-buffer" );
 			b_widget_set_font( cwin->input, "chat-input" );
+			canvas_redraw( cwin->content );
 		}
 	}
-#endif
+
 	return;
 }
 
@@ -194,7 +200,7 @@ B_OPTIONS_PAGE_SAVE( b_options_font_save )
 {
 	XMLItem *i;
 	int a;
-#if 0 /* PORTHACK */
+
 	i = c_xml_find_child( xidentity, "fonts" );
 	
 	if ( i == 0 )
@@ -204,12 +210,12 @@ B_OPTIONS_PAGE_SAVE( b_options_font_save )
 	
 	for ( a = 0; ; a++ )
 	{
-		if ( !strcmp( items[a].name, "" ) )
+		if ( !strcmp( bof_items[a].name, "" ) )
 			break;
 		
 		b_opt_fonts_set_font( i, a );
 	}
-#endif
+
 	//b_set_option_string( identity, "font", "opt_font_server_buffer", ((CTextBoxWidgetInfo *)opt_font_server_buffer->info)->text );
 }
 
